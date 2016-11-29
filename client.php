507ce73c61d4af4008efcdb711ed7edba58fbfe6
Michael Osuna <mike@plasso.com>
2016-11-29 17:26:47 +0000

    switch to regular cookies

--- client.php
+++ client.php
@@ -5,27 +5,24 @@ Class PlassoBilling {
     session_set_cookie_params(3600, '/', $_SERVER['HTTP_HOST'], true, true);
     session_start();
     $this->plassoToken = $plassoToken;
-    if(isset($plassoToken) && $plassoToken == 'logout') { $this->logout();
-    } else if($this->hasSession()) {
-      $this->plassoUserId = $_SESSION['__pl__billing']['plassoUserId'];
-      return true;
-    } else if(!$this->ping()){ $this->errorPage(); }
+    if(isset($plassoToken) && $plassoToken == 'logout') { $this->logout(); } else if(!$this->ping()){ $this->errorPage(); }
   }
   function ping() {
+    if (isset($_COOKIE['__pl_billing']) && $_COOKIE['__pl_billing'] != '') {
+      $this->plassoToken = $_COOKIE['__plasso_billing'];
+    }
     $results = file_get_contents('https://api.plasso.com/?query=%7Bmember(token%3A%22'.$this->plassoToken.'%22)%7Bid%7D%7D');
     if(!$results){ return false; } else {
       $json = json_decode($results, true);
       if(isset($json['errors']) && count($json['errors']) > 0){  $this->logout(); }
       $this->plassoUserId = $json['data']['member']['id'];
-      $_SESSION['__pl__billing'] = array('plassoUserId' => $this->plassoUserId);
+
       return true;
     }
   }
-  function hasSession() {
-    return (isset($_SESSION['__pl__billing']) && isset($_SESSION['__pl__billing']['plassoUserId']));
-  }
   function logout() {
-    unset($_SESSION['__pl__billing']);
+    unset($_COOKIE['__pl__billing']);
+    setcookie('__pl__billing', '', time() - 3600, '/', $_SERVER['HTTP_HOST'], true, true);
     echo '<html><head><meta http-equiv="refresh" content="0; URL='.((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off')?'https':'http').'://'.$_SERVER['HTTP_HOST'].'" /></head><body></body></html>';
     exit;
   }
